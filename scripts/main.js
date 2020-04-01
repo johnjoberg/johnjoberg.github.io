7a20ba72eb59aa4d2299bcd39de9d276e175e384
johnjoberg <61714674+johnjoberg@users.noreply.github.com>
2020-04-01 11:16:23 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,14 @@ myHeading.textContent = 'Hello, Fritz!';
 document.querySelector('h1').onclick = function () {
   myHeading.textContent = 'Fritz, the Frito Pie. Otherwise known as: YodaHead, Little Stink, Tiny Butt';
 }
+
+let myImage.document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAtrribute('src');
+  if(mySrc === 'images/firstImage.png') { 
+    myImage.setAtrribute('src', 'images/secondImage.png')
+  } else {
+  myImage.setAttribute('src', 'images/firstImage.png');
+  }
+}
