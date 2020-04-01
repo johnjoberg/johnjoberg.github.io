c8c37bf7900cbc1562b8b7dd665d4329e1be264a
johnjoberg <61714674+johnjoberg@users.noreply.github.com>
2020-04-01 12:21:15 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
   let myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContext = 'Mozilla is cool' + myName);
+  myHeading.textContext = 'Mozilla is cool' + myName;
 }
 
 if(!localStorage.getItem('name')) {
