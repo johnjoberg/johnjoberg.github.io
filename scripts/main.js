4ec121cf33838d83e95cc611e81e428973202183
johnjoberg <61714674+johnjoberg@users.noreply.github.com>
2020-04-01 11:51:12 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello, Fritz!';
-
-document.querySelector('h1').onclick = function () {
-  myHeading.textContent = 'Fritz, the Frito Pie. Otherwise known as: YodaHead, Little Stink, Tiny Butt';
-}
 
 let myImage.document.querySelector('img');
 
