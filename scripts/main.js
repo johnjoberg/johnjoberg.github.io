ecf3a6aed2d038e82e9c4b7e0123a963990cc0f7
johnjoberg <61714674+johnjoberg@users.noreply.github.com>
2020-04-01 13:07:39 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,15 +14,18 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  if (!myName || myName === null) {
+    setUserName();
+  } else {
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool' + myName;
+  myHeading.textContent = 'Mozilla is cool,' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName(); 
   } else {
   let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool' + storedName;
+    myHeading.textContent = 'Mozilla is cool,' + storedName;
    }
 
 myButton.onclick = function() {
